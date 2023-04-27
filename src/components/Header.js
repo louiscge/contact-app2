bd6c75d6abd3155b51038cffd3b9f7092e587e97
louiscge <louis.chague40@gmail.com>
2023-04-27 09:23:02 +0200

    react begin

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <div className="ui fixzd menu">
+            <div className="ui container center">
+                <h2>Contact Managaer</h2>
+                
+            </div>
+        </div>
+
+    );
+};
+
+export default Header;
